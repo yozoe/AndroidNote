4bc630f2767c6a17ecde10e9c79d10f39b1c7e99
wangdong <wangdong@ppzuche.com>
2016-06-21 11:15:47 +0800

    备份

--- app/src/main/java/com/yozoe/androidnote/MainActivity.java
+++ app/src/main/java/com/yozoe/androidnote/MainActivity.java
@@ -1,9 +1,10 @@
 package com.yozoe.androidnote;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.yozoe.an_common.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
