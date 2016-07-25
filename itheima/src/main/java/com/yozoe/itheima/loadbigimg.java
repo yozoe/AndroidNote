b6f595ba6fbd168983dc01b407023f8e6b780b87
wangdong <wangdong@ppzuche.com>
2016-07-25 08:47:33 +0800

    备份

---
+++ itheima/src/main/java/com/yozoe/itheima/loadbigimg.java
@@ -0,0 +1,13 @@
+package com.yozoe.itheima;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class loadbigimg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loadbigimg);
+    }
+}
