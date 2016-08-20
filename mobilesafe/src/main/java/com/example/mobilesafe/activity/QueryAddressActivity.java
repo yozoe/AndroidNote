1fb19da14132b6813a01dae1dc1026763a979ee0
wangdong <wangdong@ppzuche.com>
2016-08-20 22:34:07 +0800

    备份

--- mobilesafe/src/main/java/com/example/mobilesafe/activity/QueryAddressActivity.java
+++ mobilesafe/src/main/java/com/example/mobilesafe/activity/QueryAddressActivity.java
@@ -2,6 +2,15 @@ package com.example.mobilesafe.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,17 +26,77 @@ public class QueryAddressActivity extends Activity {
     private EditText et_phone;
     private Button bt_query;
     private TextView tv_result ;
+    private String mAddress;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            tv_result.setText(mAddress);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_query_address);
 
+        initUI();
+    }
+
+    private void initUI() {
         et_phone = (EditText) findViewById(R.id.et_phone);
         bt_query = (Button) findViewById(R.id.bt_query);
         tv_result = (TextView) findViewById(R.id.tv_result);
 
-        //测试代码,查询引擎类是否成功
-        AddressDao.getAddress("18612345678");
+        bt_query.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phone = et_phone.getText().toString();
+                if (!TextUtils.isEmpty(phone)) {
+                    //查询是耗时操作,开启子线程
+                    query(phone);
+                }
+                else {
+                    Animation shake = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.shake);
+                    //interpolator插补器,数据函数
+                    //自定义插补器
+//                    shake.setInterpolator(new Interpolator() {
+//                        @Override
+//                        public float getInterpolation(float input) {
+//                            return 0;
+//                        }
+//                    });
+
+                    et_phone.setAnimation(shake);
+                }
+            }
+        });
+
+        et_phone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String phone = et_phone.getText().toString();
+                query(phone);
+            }
+        });
+    }
+
+    protected void query(final String phone) {
+         new Thread() {
+             @Override
+             public void run() {
+                 mAddress = AddressDao.getAddress(phone);
+                 mHandler.sendEmptyMessage(0);
+             }
+         }.start();
     }
 }
--- mobilesafe/src/main/java/com/example/mobilesafe/engine/AddressDao.java
+++ mobilesafe/src/main/java/com/example/mobilesafe/engine/AddressDao.java
@@ -3,6 +3,7 @@ package com.example.mobilesafe.engine;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
+import android.util.Pair;
 
 /**
  * Created by wangdong on 16/8/18.
@@ -10,6 +11,7 @@ import android.util.Log;
 public class AddressDao {
     //1.指定访问数据库的路径
     public static String path = "/data/data/com.example.mobilesafe/files/address.db";
+    private static String mAddress = "未知号码";
 
     //传递一个电话号码,开启数据库连接进行访问,返回一个归属地
 
@@ -17,16 +19,72 @@ public class AddressDao {
      * 传递一个电话号码,开启数据连接,进行访问,返回一个归属地
      * @param phone
      */
-    public static void getAddress(String phone) {
-        phone = phone.substring(0, 7);
+    public static String getAddress(String phone) {
+        mAddress = "未知号码";
         //2.开启数据库链接,进行访问
         SQLiteDatabase db = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READONLY);
-        //3.数据库查询
-        Cursor cursor = db.query("data1", new String[]{"outkey"}, "id = ?", new String[]{phone}, null, null, null);
-        //4.查到即可
-        if (cursor.moveToNext()) {
-            String outkey = cursor.getString(0);
-            Log.i("hehe", "outkey = " + outkey);
+
+        String regularExpression = "^1[3-8]\\d{9}";
+        if (phone.matches(regularExpression)) {
+            phone = phone.substring(0, 7);
+            //3.数据库查询
+            Cursor cursor = db.query("data1", new String[]{"outkey"}, "id = ?", new String[]{phone}, null, null, null);
+            //4.查到即可
+            if (cursor.moveToNext()) {
+                String outkey = cursor.getString(0);
+                Log.i("hehe", "outkey = " + outkey);
+                //5.通过data1查询到的结果,作为外键查询data2
+                Cursor indexCursor = db.query("data2", new String[]{"location"}, "id = ?", new String[]{outkey}, null, null, null);
+                if (indexCursor.moveToNext()) {
+                    //6.获取查询到的电话归属地
+                    mAddress = indexCursor.getString(0);
+                    Log.i("hehe", mAddress);
+                }
+            }
+            else {
+                mAddress = "未知号码";
+            }
         }
+        else {
+            int length = phone.length();
+            switch (length) {
+                case 3:
+                    mAddress = "报警电话";
+                    break;
+                case 4:
+                    mAddress = "模拟器";
+                    break;
+                case 5:
+                    mAddress = "服务电话";
+                    break;
+                case 7:
+                    mAddress = "本地电话";
+                    break;
+                case 8:
+                    mAddress = "本地电话";
+                    break;
+                case 11:
+                    String area = phone.substring(1, 3);
+                    Cursor cursor = db.query("data2", new String[]{"location"}, "area = ?", new String[]{area}, null, null, null);
+                    if (cursor.moveToNext()) {
+                        mAddress = cursor.getString(0);
+                    }
+                    else {
+                        mAddress = "未知号码";
+                    }
+                    break;
+                case 12:
+                    String area1 = phone.substring(1, 4);
+                    Cursor cursor1 = db.query("data2", new String[]{"location"}, "area = ?", new String[]{area1}, null, null, null);
+                    if (cursor1.moveToNext()) {
+                        mAddress = cursor1.getString(0);
+                    }
+                    else {
+                        mAddress = "未知号码";
+                    }
+                    break;
+            }
+        }
+        return mAddress;
     }
 }
